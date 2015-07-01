db8b315bcf39250170c7eb940d34a3a482be1d04
katrin-polyakova <katrin21@ukr.net>
2015-07-02 01:00:12 +0300

    Different views for iPhons and iPads

---
+++ HomeTaskSizeClasses/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HomeTaskSizeClasses
+//
+//  Created by Kate Polyakova on 7/1/15.
+//  Copyright (c) 2015 Kate Polyakova. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+-(NSUInteger)supportedInterfaceOrientations {
+    return UIInterfaceOrientationMaskAll;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
